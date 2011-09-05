11290a4ea3581dee807ae6dc9804a3fd502629ff
crlizee <croselizee@gmail.com>
2011-09-04 21:54:32 -0400

    Completed chapter 5 exercices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+	#Returns the img path for the logo
+	def logo
+		logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+		logo
+	end
+
 	#Returns a title on a per-page basis
 	def title
 		base_title = "Ruby on Rails Tutorial Sample App"
